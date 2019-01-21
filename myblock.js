a638fff688d28751ee918c46417a58160f960911
kkh2799 <46878442+kkh2799@users.noreply.github.com>
2019-01-21 12:50:36 +0900

    Update myblock.js

--- myblock.js
+++ myblock.js
@@ -13,8 +13,8 @@
 
     var descriptor = {
         blocks: [
-            ['r', 'my test block', 'my_test_block'],
-            ['w', 'my test1 block', 'my_test1_block'],
+            ['w', 'my test block', 'my_test_block'],
+            ['r', 'my test1 block', 'my_test1_block'],
             [' ', 'my test2 block', 'my_test2_block'],
             ['R', 'my test3 block', 'my_test3_block'],
             ['h', 'my test4 block', 'my_test4_block'],
