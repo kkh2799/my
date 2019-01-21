1a95c3b445ccc8c364e66df8cbe997b43673f0f6
kkh2799 <46878442+kkh2799@users.noreply.github.com>
2019-01-21 14:15:32 +0900

    Update myblock.js

--- myblock.js
+++ myblock.js
@@ -2,6 +2,13 @@
     ext._shutdown = function() {};
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};};
+    
+    ext.wait random = function(callback){
+        time = Math.random();
+        console.log('Wating for ' + time + ' seconds');
+        window.setTimeout(function(){
+            callback();}, time*1000);};
+        
 
     ext.power = function(base, exponent) {
         return Math.pow(base, exponent);
@@ -9,7 +16,8 @@
 
     var descriptor = {
         blocks: [
-            ['r', '%n ^ %n', 'power', 2, 3],
+            ['r', '%n 의 %n 제곱', 'power', 2, 3],
+            ['w', 'wait for random time', 'wait_random'],
         ]
     };
 
