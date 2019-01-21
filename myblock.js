1593bee955cd2842a5fe7db4a4cd4865b7b3572a
kkh2799 <46878442+kkh2799@users.noreply.github.com>
2019-01-21 14:10:33 +0900

    Update myblock.js

--- myblock.js
+++ myblock.js
@@ -3,23 +3,16 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};};
 
+    ext.power = function(base, exponent) {
+        return Math.pow(base, exponent);
+    };
+
     var descriptor = {
-    blocks: [
-        ['w', 'turn motor on for %n secs',             'motorOnFor', 1],
-        [' ', 'turn motor on',                         'allMotorsOn'],
-        [' ', 'turn motor off',                        'allMotorsOff'],
-        [' ', 'set motor power %n',                    'startMotorPower', 100],
-        [' ', 'set motor direction %m.motorDirection', 'setMotorDirection', 'this way'],
-        ['h', 'when distance %m.lessMore %n',          'whenDistance', '<', 20],
-        ['h', 'when tilt %m.eNe %n',                   'whenTilt', '=', 1],
-        ['r', 'distance',                              'getDistance'],
-        ['r', 'tilt',                                  'getTilt']
-    ],
-    menus: {
-        motorDirection: ['this way', 'that way', 'reverse'],
-        lessMore: ['<', '>'],
-        eNe: ['=','not =']
-    },
-    url: 'http://info.scratch.mit.edu/WeDo',
-    displayName: 'LEGO WeDo 1.0'
-};
+        blocks: [
+            ['r', '%n ^ %n', 'power', 2, 3],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Sample extension', descriptor, ext);
+})({});
