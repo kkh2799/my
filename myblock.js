917aba6e877bd8ea3e53fc03d1eeeb0776db6cfe
kkh2799 <46878442+kkh2799@users.noreply.github.com>
2019-01-21 12:45:10 +0900

    Update myblock.js

--- myblock.js
+++ myblock.js
@@ -1,98 +1,21 @@
 (function(ext) {
-    // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
+        return {status: 2, msg: 'Ready'};};
 
-    // Block and block menu descriptions
+    ext.my_first_block = function() {};
+
     var descriptor = {
         blocks: [
-        ]
-    };
-
-    // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
-})({});
-
-(function(ext) {
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
-
-    ext.my_first_block = function() {
-        // Code that gets executed when the block is run
-    };
-
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            // Block type, block name, function name
-            ['w', 'my test block', 'my_first_block'],
+            ['r', 'my test block', 'my_test_block'],
+            ['w', 'my test1 block', 'my_test1_block'],
+            [' ', 'my test2 block', 'my_test2_block'],
+            ['R', 'my test3 block', 'my_test3_block'],
+            ['h', 'my test4 block', 'my_test4_block'],
+            ['b', 'my test5 block', 'my_test5_block'],
         ]
     };
 
     // Register the extension
     ScratchExtensions.register('My first extension', descriptor, ext);
-    
-    ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
-
-    // Functions for block with type 'w' will get a callback function as the 
-    // final argument. This should be called to indicate that the block can
-    // stop waiting.
-    ext.wait_random = function(callback) {
-        wait = Math.random();
-        console.log('Waiting for ' + wait + ' seconds');
-        window.setTimeout(function() {
-            callback();
-        }, wait*1000);
-    };
-
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            ['w', 'wait for random time1', 'wait_random'],
-        ]
-    };
-
-    // Register the extension
-    ScratchExtensions.register('Random wait extension', descriptor, ext);
-    
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
-
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
-
-    ext.power = function(base, exponent) {
-        return Math.pow(base, exponent);
-    };
-
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            // Block type, block name, function name, param1 default value, param2 default value
-            ['r', '%n ^ %n', 'power', 2, 3],
-        ]
-    };
-
-    // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
 })({});
