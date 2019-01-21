e884436179db655f1abbed973920eb2973aa9228
kkh2799 <46878442+kkh2799@users.noreply.github.com>
2019-01-21 13:38:55 +0900

    Update myblock.js

--- myblock.js
+++ myblock.js
@@ -3,7 +3,7 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};};
 
-    ext.my_test_block = function(callback) {
+    ext.my_test1_block = function(callback) {
     time = Math.random();
         console.log('Waiting for ' + time + ' seconds');
         window.setTimeout(function() {
@@ -14,7 +14,7 @@
     var descriptor = {
         blocks: [
             ['w', 'my test block', 'my_test_block'],
-            ['r', 'my test1 block', 'my_test1_block'],
+            ['r', 'wait %s seconds', 'my_test1_block'],
             [' ', 'my test2 block', 'my_test2_block'],
             ['R', 'my test3 block', 'my_test3_block'],
             ['h', 'my test4 block', 'my_test4_block'],
