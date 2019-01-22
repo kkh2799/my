c43c4dfc65f801bc42a5d09fcfebab7f29024223
kkh2799 <46878442+kkh2799@users.noreply.github.com>
2019-01-22 15:00:06 +0900

    Update test1.js

--- test1.js
+++ test1.js
@@ -4,12 +4,15 @@
         return {status: 2, msg: 'Ready'};};
     
 	
-   ext.days = function(){};
+   ext.days = function(){
+   today = new Date();
+	   return today.getDate() - base;
+   };
         
 
     var descriptor = {
         blocks: [
-	    [' ', 'days ago', 'days'],
+	    [' ', '%d days ago', 'days', 0],
         ],};
 
     ScratchExtensions.register('Sample extension', descriptor, ext);
