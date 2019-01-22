25d614b33b8eb6abc3c58faf1388652a885fd620
kkh2799 <46878442+kkh2799@users.noreply.github.com>
2019-01-22 15:23:26 +0900

    Update test1.js

--- test1.js
+++ test1.js
@@ -6,11 +6,9 @@
    
 	
    ext.days = function(base){
-   var today = new Date();
-   var dd = today.getDate();
-   var mm = today.getMonth()+1;
-   var yy = today.getFullYear();
-    return yy+'/'+mm+'/'+dd-base;
+    var today = new Date();
+    var dd = today.getDate();
+    return dd-base;
    };
         
 
