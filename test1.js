be481f891c9a20de71843d7079d9603c22f90da0
kkh2799 <46878442+kkh2799@users.noreply.github.com>
2019-01-22 09:43:57 +0900

    Create test1.js

---
+++ test1.js
@@ -0,0 +1,31 @@
+(function(ext) {
+    ext._shutdown = function() {};
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};};
+    
+    ext.wait_random = function(callback){
+        time = Math.random();
+        console.log('Wating for %d seconds', time);
+        return time;
+        window.setTimeout(function(){
+            console.log('Time');}, time*1000);};
+        
+
+    ext.power = function(base, exponent) {
+        return Math.pow(base, exponent);
+    };
+    
+    ext.wait_time = function(callback, base){
+        window.setTimeout(function(){
+            console.log('Waiting for %n seconds',base);}, base*1000);};
+        
+
+    var descriptor = {
+        blocks: [
+            ['r', '%n 의 %n 제곱', 'power', 3, 3],
+            ['w', 'wait for random time', 'wait_random'],
+            ['w', 'wait for %n seconds', 'wait_time', 1],
+        ],};
+
+    ScratchExtensions.register('Sample extension', descriptor, ext);
+})({});
