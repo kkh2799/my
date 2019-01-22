a72dafe80ca2c493349172ec6f6196cb638b1635
kkh2799 <46878442+kkh2799@users.noreply.github.com>
2019-01-22 14:49:48 +0900

    Update test1.js

--- test1.js
+++ test1.js
@@ -9,37 +9,6 @@
         window.setTimeout(function(){
             callback();}, time*1000);};
 
-	
-     ext.runMotor = function(port,speed){
-    	if(typeof port=="string"){
-			port = ports[port];
-		}
-		var deviceId = 10;
-		var extId = 0;
-		var data = [extId, 0x02, deviceId, port].concat(short2array(speed));
-		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
- 		addPackage(arrayBufferFromArray(data), function(){
- 		});
-        };
-    
-	
-	
-	
-    ext.getPirmotion = function(port,callback){
-		if(typeof port=="string"){
-			port = ports[port];
-		}
-		var deviceId = 6;
-		var extId = genNextID(port,0);
-		var data = [extId, 0x01, deviceId, port];
-		data = [data.length+3, 0xff, 0x55, data.length].concat(data);
-		_selectors["callback_"+extId] = callback;
- 		addPackage(arrayBufferFromArray(data), _selectors["callback_"+extId]);
-	};
-
-
-        
-
     ext.power = function(base, exponent) {+
         return Math.pow(base, exponent);
     };
@@ -58,8 +27,6 @@
             ['r', '%n 의 %n 제곱', 'power', 3, 3],
             ['w', 'wait for random time', 'wait_random'],
             ['w', 'wait for %d seconds', 'wait_time', 1],
-            ["R", "pir motion sensor %d.port","getPirmotion","Port2"],
-	    [" ", "set motor%d.motorPort speed %d.motorvalue","runMotor", "M1", 0],
 	    [" ", "%d days ago", "days", 0],
         ],};
 
